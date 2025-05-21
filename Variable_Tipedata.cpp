5ba4240327e56b38e1304b13bfd17b31b158fb27
NarendraNabil <narendranabilputrawardana@gmail.com>
2025-05-22 00:46:54 +0700

    penggunaan variabel dan tipe data untuk penyimpanan data sementara

---
+++ Variable_Tipedata.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    string nama = "Narendra Nabil";
+    string alamat = "Gang Sepat";
+    string kelamin = "Laki-Laki";
+    char inisial = 'N';
+    float tinggi = 167.3;
+    int umur = 19;
+    bool durian = true;
+
+    cout << "Nama saya adalah : " << nama << endl;
+    cout << "Saya tinggal di : " << alamat << endl;
+    cout << "Saya seorang : " << kelamin << endl;
+    cout << "Inisial saya : " << inisial << endl;
+    cout << "Umur saya sekarang : " << umur << endl;
+    cout << "Tinggi saya : " << tinggi << endl;
+    cout << "Saya adalah tidak suka durian : " << durian << endl;
+    return 0;
+}
