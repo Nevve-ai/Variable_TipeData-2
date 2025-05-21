6494619a3baa3f737fdf1dd4a06f14ac19e0dfed
NarendraNabil <narendranabilputrawardana@gmail.com>
2025-05-22 02:53:08 +0700

    challenge fari chatgpt

---
+++ challengebygpt.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nama = "Narendra Nabil Putra Wardana";
+    int umur = 19;
+    string hobi = "Nonton Manchester United dan Bermain Game";
+    bool coding = true;
+
+    cout << "Hai, nama saya: " << nama << endl;
+    cout << "Umur saya: " << umur << endl;
+    cout << "Hobi saya: " << hobi << endl;
+    cout << "Suka coding? " << coding << endl;
+
+    return 0;
+}
